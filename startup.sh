3c3d06a42a314be0be22b1f90dafad12d72a1030
Rodrigo <rodrigo214conceicao@gmail.com>
2019-10-05 11:30:27 +0100

    More changes...

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+
 # Simple page to use as an homepage and new tab for the browser
-python3 -m http.server --directory "$HOME/Documents/Programming/WebPages/simple-clock" 8001
+#python3 -m http.server --directory "$HOME/Documents/Programming/WebPages/simple-clock" 8001
