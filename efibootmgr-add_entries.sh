b8677dffefb01fe3f7bdf2e22b9615ae2d339b93
Rodrigo Gameiro <rodrigo214conceicao@gmail.com>
2020-02-09 23:11:19 +0000

    Changes!

--- efibootmgr-add_entries.sh
+++ efibootmgr-add_entries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=/dev/sda2 rw initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=/dev/sda3'
+efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=/dev/sda2 rw initrd=\intel-ucode.img initrd=\initramfs-linux.img resume=/dev/sda5'
 
-efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux (Fallback)" --loader /vmlinuz-linux --unicode 'root=/dev/sda2 rw initrd=\intel-ucode.img initrd=\initramfs-linux-fallback.img resume=/dev/sda3'
+efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux (Fallback)" --loader /vmlinuz-linux --unicode 'root=/dev/sda2 rw initrd=\intel-ucode.img initrd=\initramfs-linux-fallback.img resume=/dev/sda5'
 
