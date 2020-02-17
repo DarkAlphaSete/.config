010f67c4e99b98903c70aaf4519ea6955ea7b7de
Rodrigo Gameiro <rodrigo2014conceicao@gmail.com>
2020-02-17 22:43:59 +0000

    Why do I bother writing these when I'm the only one using these files?

--- set_random_wallpaper.sh
+++ set_random_wallpaper.sh
@@ -3,7 +3,7 @@
 # Get a random wallpaper if no arguments were supplied
 # Else, set a pre-set one
 if [ -z "$1" ]; then
-    find $HOME/Documents/Images/Wallpapers -type f |sort -R |tail -1 |while read file; do
+    find $HOME/Images/Wallpapers/ -type f |sort -R |tail -1 |while read file; do
 		# Set the wallpaper
 		feh --bg-fill "$file"
 		# Make pywal recalculate the colours
